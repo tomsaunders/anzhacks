0c64969ee70c0953a5e9a85c180c6ca8c9bde40c
Tom Saunders <tom@tsaunders.net>
2015-07-03 21:59:47 +1000

    fb connection
    fb app is setup to connect to anzhacks.com … requires /etc/hosts and
    local apache so far

---
+++ www/js/facebook.js
@@ -0,0 +1,66 @@
+//https://developers.facebook.com/docs/facebook-login/login-flow-for-web/v2.3
+//https://developers.facebook.com/docs/javascript/quickstart/v2.3
+
+var fbController = {
+	init: function(){
+		console.log('fb controller init');
+		$.ajaxSetup({ cache: true });
+		$.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
+			FB.init({
+				appId: '1601909030080264',
+				version: 'v2.3'
+			});
+			$('#loginbutton,#feedbutton').removeAttr('disabled');
+
+			FB.getLoginStatus(function(response) {
+				if (response.status === 'connected') {
+					console.log('Logged in.');
+				}
+				else {
+					FB.login();
+				}
+			});
+		});
+	},
+
+	// This is called with the results from from FB.getLoginStatus().
+	statusChangeCallback: function(response) {
+		console.log('statusChangeCallback');
+		console.log(response);
+		// The response object is returned with a status field that lets the
+		// app know the current login status of the person.
+		// Full docs on the response object can be found in the documentation
+		// for FB.getLoginStatus().
+		if (response.status === 'connected') {
+			// Logged into your app and Facebook.
+			fbController.testAPI();
+		} else if (response.status === 'not_authorized') {
+			// The person is logged into Facebook, but not your app.
+			$('#status').text('Please log into this app.');
+		} else {
+			// The person is not logged into Facebook, so we're not sure if
+			// they are logged into this app or not.
+			$('#status').text('Please log into Facebook');
+		}
+	},
+
+	// This function is called when someone finishes with the Login
+	// Button.  See the onlogin handler attached to it in the sample
+	// code below.
+	checkLoginState: function() {
+		FB.getLoginStatus(function(response) {
+			fbController.statusChangeCallback(response);
+		});
+	},
+
+	// Here we run a very simple test of the Graph API after login is
+	// successful.  See statusChangeCallback() for when this call is made.
+	testAPI: function() {
+		console.log('Welcome!  Fetching your information.... ');
+		FB.api('/me', function(response) {
+			console.log('Successful login for: ' + response.name);
+			document.getElementById('status').innerHTML =
+				'Thanks for logging in, ' + response.name + '!';
+		});
+	}
+}

5ca89927bce4786d2880461b8f48f22f5c8bc5ed
Nick Sonneveld <nick@sonneveld.name>
2015-07-04 15:57:45 +1000

    Add WIP AWM interface

---
+++ www/js/awm.js
@@ -0,0 +1,58 @@
+var awm = {
+	appKey: 'ww1hack2015',
+	url: 'http://www.awm.gov.au/direct/data.php',
+
+	search: function(q, start, count, type) {
+
+		var param = $.param({key: "ww1hack2015", start: start || 1, count: count || 100, q: q||"*", labels:true, format:"json"});
+		if (type) {
+			param.type = type;
+		}
+
+		// console.log(param);
+
+		var fullRequest = awm.url + "?" + param;
+		console.log("GET: " + fullRequest);
+
+		return fetch (fullRequest)
+		  .then(
+			function(response) {
+				if (response.status !== 200) {
+					throw new Exception('Looks like there was a problem. Status Code: ' + response.status)
+				}
+				return response.json();
+			}
+		)
+	},
+
+
+	searchPeople: function(q, start, count) {
+		return awm.search(q, start, count, "people");
+	},
+
+	searchUnits: function(q, start, count) {
+		return awm.search(q, start, count, "unit");
+	},
+
+	searchPlaces: function(q, start, count) {
+		return awm.search(q, start, count, "place");
+	},
+
+	searchCatalogs: function(q, start, count) {
+		return awm.search(q, start, count, "catalog");
+	},
+
+	searchRollOfHonor: function(q, start, count) {
+		return awm.search(q, start, count, "roll_of_honor");
+	},
+
+	dump: function(promprom) {
+		promprom.then(function(json) {
+			console.log(json);
+		})
+		.catch(function(err) {
+			console.log('Fetch Error bro: ', err);
+		});
+	},
+
+}

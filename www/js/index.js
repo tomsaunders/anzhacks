12bf5d9f6d65135efcb7a3c0f2a5a0b5c69ad11b
Chris Mitchell <chris@obfuscated.cx>
2015-07-05 13:22:42 +1000

    Added images to front page

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	console.log('document ready');
+	app.init();
 	quiz.init();
 	fbController.init();
 });
@@ -7,23 +8,49 @@ $(document).ready(function() {
 //basically where the main controller of the app will respond to events.
 //each data source should only talk to this controller
 var app = {
+	init: function(){
+		$('#enlist').click(function(){
+			app.startEnlistment();
+		});
+		
+		$('#dontenlist').click(function(){
+			app.dontEnlist();
+		});
+
+		$('#search').click(function(){
+			app.searchPeeps();
+		});
+	},
+	startEnlistment: function() {
+		console.log('Starting Enlistment...');
+		$("#choice").hide();
+		quiz.start(this.user);
+	},
+	dontEnlist: function() {
+		console.log('Don\'t Enlist...');
+		$("#choice").hide();
+	},
+	searchPeeps: function() {
+		console.log('Searching Peeps...');
+		$("#choice").hide();
+	},
 	loggedIn: function(){
 //		$('.jumbotron').append('<div class="alert alert-success" role="alert">You have logged in with Facebook</div>');
 		fbController.getUser();
-		fbController.getProfilePic();
+		//fbController.getProfilePic();
 	},
 	gotUser: function(user){
 		this.user = user;
 //		$('.jumbotron').append('<div class="alert alert-success" role="alert">Hello, ' + user.name + '</div>');
 //		$('.jumbotron').append('<div id="friends" class="alert alert-info" role="alert">Loading friends...</div>');
 
-		quiz.start(this.user);
+		//quiz.start(this.user);
 //		fbController.getFriends();
 //		naa.getAnzacs(this.user.last_name, this.user.first_name);
 //		awm.getWarPeeps(this.user.last_name, this.user.first_name).then(app.gotWarPeeps);
 	},
 	gotProfilePic: function(url) {
-		$('.jumbotron').append('<div id="profile"><img class="sepia" src="' + url + '""></div>');
+		//$('.jumbotron').append('<div id="profile"><img class="sepia" src="' + url + '""></div>');
 	},
 	gotFriends: function(friends){
 		$('#friends').text(friends.length + ' friends loaded');
